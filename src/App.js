a11d90ed9ffc9449c3f969887d8f8da284e534f2
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-09-24 13:13:52 +0600

    css design modify

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { RightPanel } from "./components/rightPanel/RightPanel";
 
 function App() {
   return (
-    <div className="bg-gray-200 flex flex-row">
-        <LeftPanel />
-        <RightPanel />
+    <div className="container mx-auto bg-gray-200">
+       <div className="flex">
+       <LeftPanel />
+       <RightPanel />
+       </div>
     </div>
     
 
--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -3,8 +3,10 @@ const ProductCard = ({productImage,productName, productPrice}) => {
   return (
     <div className="bg-white p-4 rounded shadow">
         <img src={productImage} alt="product" width="300px" />
-        <h1 className="text-lg font-semibold">{productName}</h1>
-        <p>{`$${productPrice}`}</p>
+       <div className="flex justify-between">
+       <h1 className="text-lg font-semibold">{productName}</h1>
+       <p>{`$${productPrice}`}</p>
+       </div>
         <button className="bg-blue-500 hover:bg-blue-700
          text-white font-bold py-2 px-2
           rounded mt-2">Add to Cart</button>
