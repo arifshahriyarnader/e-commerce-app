ecf0c489c59924f846eaccdb68a927acc0a9deac
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-09-25 14:47:57 +0600

    margin padding fixed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { RightPanel } from "./components/rightPanel/RightPanel";
 
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200">
-       <div className="flex">
-       <LeftPanel />
-       <RightPanel />
-       </div>
+    <div className="bg-gray-200">
+      <div className="flex container mx-auto">
+      <LeftPanel />
+      <RightPanel />
+        </div>   
     </div>
     
 
