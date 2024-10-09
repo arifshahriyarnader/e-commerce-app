f39dfe2a4dee56dc6bbf0110867c356a0535ddb9
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-10-09 23:38:33 +0600

    Move useEffect logics  to custom hooks

--- client/src/api/queries/useProduct.jsx
+++ client/src/api/queries/useProduct.jsx
@@ -1,7 +1,18 @@
-import { productServices } from "../services"
+import axios from "axios";
+import { useEffect, useState } from "react"
+//import { productServices } from "../services"
 
 export const useProduct = () => {
-    const products = productServices.getProducts();
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/products').then((res) =>{
+      setProducts(res.data);
+    })
+    .catch((err) =>{
+      alert('Failed to load Products')
+      console.error('Error Fetching products:', err);
+    })
+  }, [])
       return {products}
 }
 
--- client/src/components/product/ProductGrid.jsx
+++ client/src/components/product/ProductGrid.jsx
@@ -1,20 +1,8 @@
-import { useEffect, useState } from "react";
-import axios from 'axios';
-//import { useProduct } from "../../api/queries";
+import { useProduct } from "../../api/queries/useProduct";
 import ProductCard from "./ProductCard";
 
 export const ProductGrid = () => {
-  const [products, setProducts] = useState([])
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/products').then((res) => {
-      setProducts(res.data)
-    })
-    .catch((err) =>{
-      alert('Failed to load products')
-      console.error('Error Fetching products:', err);
-    })
-  }, [])
-  //const {products} = useProduct();
+   const {products} =useProduct();
   return (
     <div className="p-8 grid grid-cols-3 gap-4">
       {products.map((product, index) => {
