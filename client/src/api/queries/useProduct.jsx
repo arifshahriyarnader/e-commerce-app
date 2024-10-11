c6a50bdec35d984f21272d39500d5dc49134ac5a
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-10-11 22:22:16 +0600

    Add Loading state while fetching data

--- client/src/api/queries/useProduct.jsx
+++ client/src/api/queries/useProduct.jsx
@@ -2,22 +2,25 @@ import { useEffect, useState } from "react"
 import { productServices } from "../services"
 
 export const useProduct = () => {
+  const [isLoading, setIsLoading] = useState(false)
   const [products, setProducts] = useState([])
-
   const loadProducts = async() =>{
     try{
+      setIsLoading(true)
       const products= await productServices.getProducts();
       setProducts(products);
     }
     catch(err){
       alert("Failed to load products")
       console.log(err)
+    } finally{
+      setIsLoading(false)
     }
   }
   useEffect(() => {
     loadProducts()
   }, [])
-      return {products}
+      return {isLoading,products}
 }
 
 
--- client/src/components/product/ProductGrid.jsx
+++ client/src/components/product/ProductGrid.jsx
@@ -2,12 +2,14 @@ import { useProduct } from "../../api/queries/useProduct";
 import ProductCard from "./ProductCard";
 
 export const ProductGrid = () => {
-   const {products} =useProduct();
+  
+   const {isLoading,products} =useProduct();
   return (
     <div className="p-8 grid grid-cols-3 gap-4">
-      {products.map((product, index) => {
+      {isLoading ? <h1>Loading....</h1> : products.map((product, index) => {
         return <ProductCard key={index} product={product} />;
       })}
+     
     </div>
   );
 };
