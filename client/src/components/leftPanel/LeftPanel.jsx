7d2c4b8ff48d9784a870c896c0e5fbf5480c0348
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-10-17 20:34:05 +0600

    Add MUI components and icon library

--- client/src/components/leftPanel/LeftPanel.jsx
+++ client/src/components/leftPanel/LeftPanel.jsx
@@ -1,10 +1,8 @@
-//import ProductGrid from "../product/ProductGrid"
 import ProductGrid from "../product/ProductGrid";
 import { FilterSection } from "./FilterSection"
 import { LeftPanelHeader } from "./LeftPanelHeader";
 
 export const LeftPanel=() =>{
-  
   const headerTitle="React E-Commerce App";
     return (
       <div>
