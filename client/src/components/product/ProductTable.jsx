18ad13814dad2f7422979ee03ac0ee93ce5d6d8b
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-11-02 20:46:21 +0600

    Add value formatter in inventory page

--- client/src/components/product/ProductTable.jsx
+++ client/src/components/product/ProductTable.jsx
@@ -1,49 +1,56 @@
-import { useProduct } from '../../api/queries';
-import { Box, Table, TableEmptyState, } from '../../common/components';
+import { useProduct } from "../../api/queries";
+import { Box, Table, TableEmptyState } from "../../common/components";
 
 export const ProductTable = () => {
-  const {isLoading, products} = useProduct();
+  const { isLoading, products } = useProduct();
 
-  const getFormattedRows =() => products.map(product =>({
-    id:product.id,
-    name:product.name,
-    price:product.price,
-    quantity:product.quantity
-  }))
+  const getFormattedRows = () =>
+    products.map((product) => ({
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      quantity: product.quantity,
+    }));
 
   return (
     <Box py={4}>
-     <Table columns={[
-      {
-        field:'name',
-        headerName:'Product Name',
-        flex:1
-    },
-    {
-      field:'price',
-      headerName:'Price',
-      headerAlign:'center',
-      type:'number',
-      width:150
-    },
-    {
-      field:'quantity',
-      headerName:'Quantity',
-      headerAlign:'center',
-      width:150,
-      type:'number'
-    },
-     ]} rows={getFormattedRows()} loading={isLoading} paginationMode='client'
-     initialState={{
-      pagination:{paginationModel: {pageSize: 5}},
-     }}
-     pageSizeOptions={[5,10,15]}
-     slots={{
-      noRowsOverlay:() => <TableEmptyState />,
-     }}
-     sx={{
-      minHeight:400,}} />
+      <Table
+        columns={[
+          {
+            field: "name",
+            headerName: "Product Name",
+            flex: 1,
+          },
+          {
+            field: "price",
+            headerName: "Price",
+            headerAlign: "center",
+            type: "number",
+            width: 150,
+            valueFormatter: (value) => `$${value}`,
+          },
+          {
+            field: "quantity",
+            headerName: "Quantity",
+            headerAlign: "center",
+            width: 150,
+            type: "number",
+          },
+        ]}
+        rows={getFormattedRows()}
+        loading={isLoading}
+        paginationMode="client"
+        initialState={{
+          pagination: { paginationModel: { pageSize: 5 } },
+        }}
+        pageSizeOptions={[5, 10, 15]}
+        slots={{
+          noRowsOverlay: () => <TableEmptyState />,
+        }}
+        sx={{
+          minHeight: 400,
+        }}
+      />
     </Box>
-  )
-}
-
+  );
+};
